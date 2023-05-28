326ec67193ff34a6ad8b11ff66633d029cce4238
VIGNESH NAGAVEL <72061699+vigneshnitthish@users.noreply.github.com>
2023-05-28 14:10:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import { useState } from 'react';
-// import { auth } from './firebase';
-// import Home from "./Home"
+import { useState } from 'react';
+import { auth } from './firebase';
+import Home from "./Home"
 import Login from './Login';
-// import Register from './Register';
-// import AdminLogin from "./Adminlogin"
-// import Admin from "./Admin"
-
+import Register from './Register';
+import AdminLogin from "./Adminlogin"
+import Admin from "./Admin"
+import { BrowserRouter } from "react-router-dom";
 
 
 function App() {
@@ -14,12 +14,15 @@ function App() {
 
   return (
     
-    <Router>
+    <BrowserRouter>
       <Routes>
-        <Route exact path="communication-video/" element={<Login />} />
-        
+        <Route path="communication-video/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path = "/home" element = {<Home/>}/>
+        <Route path = "/adminlogin" element = {<AdminLogin/>}/>
+        <Route path = "/admin" element = {<Admin/>}/>
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
