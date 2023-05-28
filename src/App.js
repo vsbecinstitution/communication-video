ec1fb15366615f69860432ebdb49358f7dbbb0dd
VIGNESH NAGAVEL <72061699+vigneshnitthish@users.noreply.github.com>
2023-05-28 13:28:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-const App = ()=>{
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { useState } from 'react';
+// import { auth } from './firebase';
+// import Home from "./Home"
+import Login from './Login';
+// import Register from './Register';
+// import AdminLogin from "./Adminlogin"
+// import Admin from "./Admin"
+import { BrowserRouter } from "react-router-dom";
 
-    return (<h1>Vignesh</h1>)
 
+function App() {
+  
+
+  return (
+    
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
