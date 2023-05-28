b05ff2563e46ccfefa3ffb61943e6c701d84b48f
VIGNESH NAGAVEL <72061699+vigneshnitthish@users.noreply.github.com>
2023-05-28 13:05:00 +0530

    index

--- src/index.js
+++ src/index.js
@@ -3,12 +3,29 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Login from './Login';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
+import { auth } from './firebase';
+import Home from "./Home"
+import Login from './Login';
+import Register from './Register';
+import AdminLogin from "./Adminlogin"
+import Admin from "./Admin"
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
   <React.StrictMode>
-    <App/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path = "/home" element = {<Home/>}/>
+        <Route path = "/adminlogin" element = {<AdminLogin/>}/>
+        <Route path = "/admin" element = {<Admin/>}/>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
